aa7574c0e805669d5c9b6460c7d7821dc78540a1
Iiro Kiviluoma <iiro.kiviluoma@tuni.fi>
2020-05-07 12:34:16 +0300

    1.7 valmis.

--- osa1/unicafe/src/index.js
+++ osa1/unicafe/src/index.js
@@ -4,12 +4,30 @@ import ReactDOM from 'react-dom'
 
 // Yhteenveto
 const Statistics = (props) => {
+  // Lasketaan keskiarvo
+  let total = 0
+  let pos_total = 0
+
+  props.all.forEach(x => {
+    total += x
+
+    if (x === 1) {
+      pos_total++
+    }
+  })
+
+  const average = total / props.all.length
+  const positive = pos_total / props.all.length
+
   return (
     <div>
       <h2>Statistics</h2>
       <p>Good: {props.good}</p>
       <p>Neutral: {props.neutral}</p>
       <p>Bad: {props.bad}</p>
+      <p>Feedbacks: {props.all.length}</p>
+      <p>Average: {average}</p>
+      <p>Positive: {positive}%</p>
     </div>
   )
 }
@@ -30,10 +48,21 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [all, setAll] =useState([])
 
-  const handleGoodFeedback = () => setGood(good + 1)
-  const handleNeutralFeedback = () => setNeutral(neutral + 1)
-  const handleBadFeedback = () => setBad(bad + 1)
+  const handleGoodFeedback = () => {
+    setAll(all.concat(1))
+    setGood(good + 1)
+  }
+  const handleNeutralFeedback = () => {
+    setAll(all.concat(0))
+    setNeutral(neutral + 1)
+  }
+
+  const handleBadFeedback = () => {
+    setAll(all.concat(-1))
+    setBad(bad + 1)
+  }
 
   return (
     <div>
@@ -41,7 +70,7 @@ const App = () => {
       <Button onClick={handleGoodFeedback} text='Good'/>
       <Button onClick={handleNeutralFeedback} text='Neutral'/>
       <Button onClick={handleBadFeedback} text='Bad'/>
-      <Statistics good={good} neutral={neutral} bad={bad}/>
+      <Statistics good={good} neutral={neutral} bad={bad} all={all}/>
     </div>
   )
 }
