a69d086a4bbc11b194cd866ee70b6133e814035d
Iiro Kiviluoma <iiro.kiviluoma@tuni.fi>
2020-07-03 21:28:06 +0300

    4.5* valmis.

--- osa4/blogilista/tests/list_helper.test.js
+++ osa4/blogilista/tests/list_helper.test.js
@@ -1,5 +1,77 @@
 const listHelper = require('../utils/list_helper')
 
+const listOneBlog = [
+  {
+    _id: '5a422a851b54a676234d17f7',
+    title: 'Test automation',
+    author: 'Teppo Testaaja',
+    url: 'www.testiblogi1.com',
+    likes: 5,
+    __v: 0
+  }
+]
+
+const listMultipleBlogs = [
+  {
+    _id: '5a422a851b54a676234d17f7',
+    title: 'Test automation',
+    author: 'Teppo Testaaja',
+    url: 'www.testiblogi1.com',
+    likes: 5,
+    __v: 0
+  },
+  {
+    _id: '5a422aa71b54a676234d17f8',
+    title: 'Testaajan blogi',
+    author: 'Matti Mäkinen',
+    url: 'www.testiblogi2.com',
+    likes: 2,
+    __v: 0
+  },
+  {
+    _id: '5a422b3a1b54a676234d17f9',
+    title: 'Mä joka päivä testejä teen',
+    author: 'Jose Mattila',
+    url: 'www.testiblogi3.com',
+    likes: 9,
+    __v: 0
+  },
+  {
+    _id: '5a422b891b54a676234d17fa',
+    title: 'Kiven kovaa testausta',
+    author: 'Timo Testimies',
+    url: 'www.testiblogi4.com',
+    likes: 1,
+    __v: 0
+  },
+  {
+    _id: '5a422ba71b54a676234d17fb',
+    title: 'Testing for dummies',
+    author: 'Tuija Testinainen',
+    url: 'www.testiblogi5.com',
+    likes: 18,
+    __v: 0
+  },
+  {
+    _id: '5a422bc61b54a676234d17fc',
+    title: 'Testiblogi',
+    author: 'Heikki Testihenkilö',
+    url: 'www.testiblogi6.com',
+    likes: 12,
+    __v: 0
+  }
+]
+
+const blogMostLikes =
+  {
+    _id: '5a422ba71b54a676234d17fb',
+    title: 'Testing for dummies',
+    author: 'Tuija Testinainen',
+    url: 'www.testiblogi5.com',
+    likes: 18,
+    __v: 0
+  }
+
 test('Dummy returns one', () => {
   const blogs = []
 
@@ -7,69 +79,7 @@ test('Dummy returns one', () => {
   expect(testResult).toBe(1)
 })
 
-describe('Total likes:', () => {
-  const listOneBlog = [
-    {
-      _id: '5a422a851b54a676234d17f7',
-      title: 'Test automation',
-      author: 'Teppo Testaaja',
-      url: 'www.testiblogi1.com',
-      likes: 5,
-      __v: 0
-    }
-  ]
-
-  const listMultipleBlogs = [
-    {
-      _id: '5a422a851b54a676234d17f7',
-      title: 'Test automation',
-      author: 'Teppo Testaaja',
-      url: 'www.testiblogi1.com',
-      likes: 5,
-      __v: 0
-    },
-    {
-      _id: '5a422aa71b54a676234d17f8',
-      title: 'Testaajan blogi',
-      author: 'Matti Mäkinen',
-      url: 'www.testiblogi2.com',
-      likes: 2,
-      __v: 0
-    },
-    {
-      _id: '5a422b3a1b54a676234d17f9',
-      title: 'Mä joka päivä testejä teen',
-      author: 'Jose Mattila',
-      url: 'www.testiblogi3.com',
-      likes: 9,
-      __v: 0
-    },
-    {
-      _id: '5a422b891b54a676234d17fa',
-      title: 'Kiven kovaa testausta',
-      author: 'Timo Testimies',
-      url: 'www.testiblogi4.com',
-      likes: 1,
-      __v: 0
-    },
-    {
-      _id: '5a422ba71b54a676234d17fb',
-      title: 'Testing for dummies',
-      author: 'Tuija Testinainen',
-      url: 'www.testiblogi5.com',
-      likes: 18,
-      __v: 0
-    },
-    {
-      _id: '5a422bc61b54a676234d17fc',
-      title: 'Testiblogi',
-      author: 'Heikki Testihenkilö',
-      url: 'www.testiblogi6.com',
-      likes: 12,
-      __v: 0
-    }
-  ]
-
+describe('Total likes', () => {
   test('Empty list has total likes of zero', () => {
     const result = listHelper.totalLikes([])
     expect(result).toBe(0)
@@ -83,5 +93,22 @@ describe('Total likes:', () => {
   test('Total likes of multiple blogs is calculated right', () => {
     const result = listHelper.totalLikes(listMultipleBlogs)
     expect(result).toBe(47)
+  })
+})
+
+describe('Favorite blogs', () => {
+  test('Empty list has no favorite blog', () => {
+    const result = listHelper.favoriteBlog([])
+    expect(result).toEqual(0)
+  })
+
+  test('One blog must be favorite blog', () => {
+    const result = listHelper.favoriteBlog(listOneBlog)
+    expect(result).toEqual(listOneBlog[0])
+  })
+
+  test('Favorite blog from multiple blogs is the one with most likes', () => {
+    const result = listHelper.favoriteBlog(listMultipleBlogs)
+    expect(result).toEqual(blogMostLikes)
   })
 })
--- osa4/blogilista/utils/list_helper.js
+++ osa4/blogilista/utils/list_helper.js
@@ -5,11 +5,18 @@ const dummy = (blogs) => {
 
 const totalLikes = (blogs) => {
   const reducer = (total, blog) => total + blog.likes
-
   return blogs.reduce(reducer, 0)
+}
+
+const favoriteBlog = (blogs) => {
+  if (blogs.length === 0) return 0
+
+  const reducer = (prev, cur) => cur.likes > prev.likes ? cur : prev
+  return blogs.reduce(reducer)
 }
 
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favoriteBlog
 }
