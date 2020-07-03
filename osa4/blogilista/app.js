bcb45f940580da76c3ff93313431d0261043ea0f
Iiro Kiviluoma <iiro.kiviluoma@tuni.fi>
2020-07-03 19:52:10 +0300

    osa4: index ja app hajautettu osiin.

---
+++ osa4/blogilista/app.js
@@ -0,0 +1,27 @@
+const config = require('./utils/config')
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const blogsRouter = require('./controllers/blogs')
+const middleware = require('./utils/middleware')
+const logger = require('./utils/logger')
+const mongoose = require('mongoose')
+
+const url = config.MONGODB_URL
+
+logger.info(`Connecting to MongoDB: ${url}`)
+mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true})
+  .then(() => {
+    logger.info(`Connected to MongoDB.`)
+  })
+  .catch((error) => {
+    logger.error(`Connection error ${error.message}`)
+  })
+
+app.use(cors())
+app.use(express.json())
+app.use(middleware.requestLogger)
+
+app.use('/api/blogs', blogsRouter)
+
+module.exports = app
--- osa4/blogilista/index.js
+++ osa4/blogilista/index.js
@@ -1,30 +1,11 @@
+const app = require('./app')
+const http = require('http')
 const config = require('./utils/config')
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const blogsRouter = require('./controllers/blogs')
-const middleware = require('./utils/middleware')
 const logger = require('./utils/logger')
-const mongoose = require('mongoose')
 
-const url = config.MONGODB_URL
-
-logger.info(`Connecting to MongoDB: ${url}`)
-mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true})
-  .then(() => {
-    logger.info(`Connected to MongoDB.`)
-  })
-  .catch((error) => {
-    logger.error(`Connection error ${error.message}`)
-  })
-
-app.use(cors())
-app.use(express.json())
-app.use(middleware.requestLogger)
-
-app.use('/api/blogs', blogsRouter)
+const server = http.createServer(app)
 
 const PORT = config.PORT
-app.listen(PORT, () => {
-  logger.info(`Server running on port ${PORT}`)
+server.listen(PORT, () => {
+  logger.info(`Server running - PORT: ${PORT}`)
 })
