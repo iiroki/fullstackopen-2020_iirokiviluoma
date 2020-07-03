e47c4258bff9424c5a61bad5bec1ae9e9f3235e8
Iiro Kiviluoma <iiro.kiviluoma@tuni.fi>
2020-07-03 20:04:33 +0300

    4.1 ja 4.2 valmiit: Sovellus toimiva npm-projekti, joka on jaettu moduuleihin.

--- osa4/blogilista/app.js
+++ osa4/blogilista/app.js
@@ -1,14 +1,15 @@
-const config = require('./utils/config')
 const express = require('express')
 const app = express()
 const cors = require('cors')
 const blogsRouter = require('./controllers/blogs')
+const config = require('./utils/config')
 const middleware = require('./utils/middleware')
 const logger = require('./utils/logger')
 const mongoose = require('mongoose')
 
 const url = config.MONGODB_URL
 
+// Yhdistetään MongoDB-tietokantaan
 logger.info(`Connecting to MongoDB: ${url}`)
 mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true})
   .then(() => {
@@ -22,6 +23,7 @@ app.use(cors())
 app.use(express.json())
 app.use(middleware.requestLogger)
 
+// Käytetään omaa reittien käsittelijää
 app.use('/api/blogs', blogsRouter)
 
 module.exports = app
--- osa4/blogilista/utils/middleware.js
+++ osa4/blogilista/utils/middleware.js
@@ -1,7 +1,6 @@
 const logger = require('./logger')
 
 const requestLogger = (request, response, next) => {
-  logger.info(`-----`)
   logger.info(`Method: ${request.method}`)
   logger.info(`Path:   ${request.path}`)
   logger.info(`Body:   ${JSON.stringify(request.body)}`)
