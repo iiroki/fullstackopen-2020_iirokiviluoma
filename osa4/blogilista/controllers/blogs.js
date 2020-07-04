c89c128f31f27f8928c4b3ec44dd8788a186027c
Iiro Kiviluoma <iiro.kiviluoma@tuni.fi>
2020-07-04 16:23:21 +0300

    4.8: Routet käyttävät async/awaitia.

--- osa4/blogilista/controllers/blogs.js
+++ osa4/blogilista/controllers/blogs.js
@@ -2,35 +2,29 @@ const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 
 // Haetaan kaikki blogit
-blogsRouter.get('/', (request, response) => {
-  Blog.find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
+blogsRouter.get('/', async (request, response) => {
+  const blogs = await Blog.find({})
+  response.json(blogs)
 })
 
-blogsRouter.get('/:id', (request, response, next) => {
-  Blog.findById(request.params.id)
-    .then(blog => {
-      if (blog) {
-        response.json(blog)
-      }
-      else {
-        response.status(404).end()
-      }
-    })
-    .catch(error => next(error))
+// Haetaan yksittäinen blogi
+blogsRouter.get('/:id', async (request, response, next) => {
+  const blog = await Blog.findById(request.params.id)
+
+  // Tarkistetaan löytyikö blogi tietokannasta
+  if (blog) {
+    response.json(blog)
+  }
+  else {
+    response.status(404).end()
+  }
 })
 
 // Lisätään uusi blogi
-blogsRouter.post('/', (request, response, next) => {
-  const newBlog = new Blog(request.body)
-
-  newBlog.save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-    .catch(error => next(error))
+blogsRouter.post('/', async (request, response, next) => {
+  const blog = new Blog(request.body)
+  const savedBlog = await blog.save()
+  response.status(201).json(savedBlog)
 })
 
 module.exports = blogsRouter
