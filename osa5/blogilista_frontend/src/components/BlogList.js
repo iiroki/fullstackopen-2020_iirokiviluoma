07f3fe765b88a412b843843c19845db0e45c6217
Iiro Kiviluoma <iiro.kiviluoma@tuni.fi>
2020-07-08 11:35:56 +0300

    5.7* valmis. FullViewin yhteydessä huomattu, että uuden blogin lisännyt käyttäjä ei päivity suoraan lisäämisen yhteydessä, ainoastaan getAll() jälkeen. Täytyy korjata.

--- osa5/blogilista_frontend/src/components/BlogList.js
+++ osa5/blogilista_frontend/src/components/BlogList.js
@@ -1,10 +1,37 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} - {blog.author}
+const BlogView = ({ blog, handleFullView }) => (
+  <div className='blog'>
+      {blog.title} - {blog.author} 
+      <button className='button' onClick={handleFullView}>View</button>
+    </div>
+)
+
+const BlogFullView = ({ blog, handleFullView }) => (
+  <div className='blog'>
+    <b>{blog.title}</b>
+    <button className='button' onClick={handleFullView}>Hide</button><br/>
+    Author: {blog.author}<br/>
+    Likes: {blog.likes}
+    <button className='button'>Like</button><br/>
+    Added by: {blog.user.name}<br/>
   </div>
 )
+
+const Blog = ({ blog }) => {
+  const [fullView, setFullView] = useState(false)
+
+  const toggleFullView = () => {
+    setFullView(!fullView)
+  }
+
+  // Näytetään blogin tiedot fullView-muuttujan mukaan
+  return (
+    fullView === true
+      ? <BlogFullView blog={blog} handleFullView={toggleFullView}/>
+      : <BlogView blog={blog} handleFullView={toggleFullView} />
+  )
+}
 
 const BlogList = ({ blogs }) => (
   <div className='blogList'>
