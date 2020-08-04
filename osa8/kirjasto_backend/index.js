56426a11cdff1ba7b96dd33a68f6c4f38044dbcb
Iiro Kiviluoma <iirokiviluoma@outlook.com>
2020-08-04 13:30:40 +0300

    8.3 valmis.

--- osa8/kirjasto_backend/index.js
+++ osa8/kirjasto_backend/index.js
@@ -84,6 +84,13 @@ let books = [
 ]
 
 const typeDefs = gql`
+  type Author {
+    name: String!
+    born: Int
+    id: ID!
+    bookCount: Int
+  }
+
   type Book {
     title: String!
     author: String!
@@ -96,6 +103,7 @@ const typeDefs = gql`
     bookCount: Int!
     authorCount: Int!
     allBooks: [Book!]!
+    allAuthors: [Author!]!
   }
 `
 
@@ -103,7 +111,11 @@ const resolvers = {
   Query: {
     bookCount: () => books.length,
     authorCount: () => authors.length,
-    allBooks: () => books
+    allBooks: () => books,
+    allAuthors: () => authors
+  },
+  Author: {
+    bookCount: (root) => books.filter(b => b.author === root.name).length
   }
 }
 
