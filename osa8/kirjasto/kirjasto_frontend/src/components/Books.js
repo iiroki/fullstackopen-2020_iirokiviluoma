e1f23bb4585f0b487348916c5343c7e9101da45b
Iiro Kiviluoma <iirokiviluoma@outlook.com>
2020-08-19 15:45:50 +0300

    8.19 valmis.

--- osa8/kirjasto/kirjasto_frontend/src/components/Books.js
+++ osa8/kirjasto/kirjasto_frontend/src/components/Books.js
@@ -1,9 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useQuery } from '@apollo/client'
 
 import { ALL_BOOKS } from '../queries'
 
+const GenreButton = ({ genre, setFilter }) => (
+  <button onClick={() => setFilter(genre)}>{genre}</button>
+)
+
 const Books = (props) => {
+  const [genreFilter, setGenreFilter] = useState('')
   const result = useQuery(ALL_BOOKS)
 
   if (!props.show) {
@@ -16,11 +21,24 @@ const Books = (props) => {
     )
   }
 
-  const books = result.data.allBooks
+  const allBooks = result.data.allBooks
+
+  const genresCombined = [].concat.apply([], allBooks.map(b => b.genres))
+  const genres = [...new Set(genresCombined)]
+
+  const books = genreFilter === ''
+    ? allBooks
+    : allBooks.filter(b => b.genres.includes(genreFilter))
 
   return (
     <div>
       <h2>books</h2>
+
+      Filter: {genreFilter} <br/>
+      <button onClick={() => setGenreFilter('')}><b>X</b></button>
+      {genres.map(g => 
+        <GenreButton key={g} genre={g} setFilter={setGenreFilter} />
+      )}
 
       <table>
         <tbody>
